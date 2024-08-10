ff4872a57dca8ec13f3d5e5efaedced894504b1a
Aidar Garikhanov <a1d4r@yandex.ru>
2024-08-10 17:03:35 +0300

    Add faststream users example

---
+++ notifications-worker/tests/test_users.py
@@ -0,0 +1,19 @@
+import pydantic
+import pytest
+
+from faststream.rabbit import TestRabbitBroker
+
+from notifications_worker.main import broker
+
+
+@pytest.mark.asyncio()
+async def test_correct():
+    async with TestRabbitBroker(broker) as br:
+        await br.publish({"user": "John", "user_id": 1}, "in-queue")
+
+
+@pytest.mark.asyncio()
+async def test_invalid():
+    async with TestRabbitBroker(broker) as br:
+        with pytest.raises(pydantic.ValidationError):
+            await br.publish("wrong message", "in-queue")
