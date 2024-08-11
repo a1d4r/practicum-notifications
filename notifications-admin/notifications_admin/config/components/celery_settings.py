8c69a9bd1bd4a291b0e80fc48ecc5def0f6e387f
FoxyAn <118219124+Fox13th@users.noreply.github.com>
2024-08-12 00:26:17 +0300

    Update celery_settings.py

--- notifications-admin/notifications_admin/config/components/celery_settings.py
+++ notifications-admin/notifications_admin/config/components/celery_settings.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 RABBIT_USER = os.environ.get('RABBIT_USER')
 RABBIT_PASS = os.environ.get('RABBIT_PASS')
+RABBIT_HOST = os.environ.get('RABBIT_HOST')
 
-CELERY_BROKER_URL = f'amqp://{RABBIT_USER}:{RABBIT_PASS}@localhost//'
+CELERY_BROKER_URL = f'amqp://{RABBIT_USER}:{RABBIT_PASS}@{RABBIT_HOST}//'
 
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
